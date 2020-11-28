b07ea152c10e9601bc1dd74e65ff5628cd0cda2e
wlazlok <Karo1l2318>
2020-11-28 16:07:04 +0100

    user repository refactoring

--- src/main/java/app/invoice/repositories/UserRepository.java
+++ src/main/java/app/invoice/repositories/UserRepository.java
@@ -1,26 +1,19 @@
 package app.invoice.repositories;
 
 import app.invoice.models.User;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-
+@Transactional
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    @Transactional
-    @Modifying
     void deleteUserByIdGreaterThanEqual(Long id);
 
     User getById(Long id);
 
     User getByUserName(String userName);
 
-    @Modifying
-    @Transactional
     User save(User user);
 
-    @Modifying
-    @Transactional
     void deleteById(Long id);
 }
